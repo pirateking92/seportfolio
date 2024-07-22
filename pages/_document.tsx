8feae7abcd96daaab5bdf424c5562539dd227a7d
Matt Doyle <doyle9214@gmail.com>
2024-07-22 14:57:40 +0100

    have added some styling to the background and font, only color at the moment, will play around with profile pic now

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,28 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// pages/_document.tsx
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { JSXElementConstructor, ReactElement } from 'react';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const originalRenderPage = ctx.renderPage;
+
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render(): ReactElement<any, string | JSXElementConstructor<any>> {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,6 @@
 // pages/about.tsx
 import { GetStaticProps } from 'next';
-import { gql } from '@apollo/client';
+import Head from 'next/head';
 import client from '../apollo-client';
 import { GET_ABOUT_PAGE } from '../queries';
 import Image from 'next/image';
@@ -13,17 +13,24 @@ interface AboutPageProps {
 
 const About: React.FC<AboutPageProps> = ({ title, content, featuredImage }) => {
   return (
-    <div>
-      <h1>{title}</h1>
+    <div className="container mx-auto p-4 ">
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content="About Sepy Baghaei" />
+      </Head>
+      <h1 className="text-4xl text-justify font-bold mb-4 text-slate-300">{title}</h1>
       {featuredImage && (
-        <Image
-          src={featuredImage}
-          alt={title}
-          width={600}
-          height={400}
-        />
+        <div className="mb-4">
+          <Image
+            src={featuredImage}
+            alt={title}
+            width={600}
+            height={400}
+            className="rounded"
+          />
+        </div>
       )}
-      <div dangerouslySetInnerHTML={{ __html: content }} />
+      <div className="prose text-slate-300" dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   );
 };
