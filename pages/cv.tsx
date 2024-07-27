cc916bfd04c9b4faec712e38b844b652052d8aba
Matt Doyle <doyle9214@gmail.com>
2024-07-27 22:38:45 +0100

    combined queries are a pain, will separate them to make it easier to manage, and edit all the respective files to make it so it works. then sepy will be able to upload a cv and make it work

--- pages/cv.tsx
+++ pages/cv.tsx
@@ -1,32 +1,51 @@
-// pages/cv.tsx
+import { GetStaticProps } from "next";
 import Head from "next/head";
-import Image from "next/image";
-import Navbar from "../components/Navbar";
+import client from "../apollo-client";
+import { GET_SITE_SETTINGS } from "../queries";
 
-const CVPage: React.FC = () => {
+interface CVPageProps {
+  cvUrl: string;
+}
+
+const CVPage: React.FC<CVPageProps> = ({ cvUrl }) => {
   return (
-    <div className="flex min-h-screen flex-col">
-      <Navbar />
+    <div className="container mx-auto p-4">
       <Head>
         <title>My CV</title>
-        <meta name="description" content="CV" />
+        <meta name="description" content="Curriculum Vitae" />
       </Head>
       <div className="flex flex-col items-center">
-        <h1 className="text-4xl text-slate-300 font-bold mb-4 text-center py-3">
+        <h1 className="text-4xl text-slate-300 font-bold mb-4 text-center">
           My CV
         </h1>
-        <div className="mx-auto">
-          <Image
-            src="/SEPY BAGHAEI CV 2024-1.png" // Path to the CV image in the public directory
-            alt="My CV"
-            width={800} // Specify the width of the image
-            height={1131} // Specify the height of the image
+        {cvUrl ? (
+          <embed
+            src={cvUrl}
+            type="application/pdf"
+            width="800"
+            height="1131"
             className="rounded ring"
           />
-        </div>
+        ) : (
+          <p className="text-slate-300">CV not available.</p>
+        )}
       </div>
     </div>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await client.query({
+    query: GET_SITE_SETTINGS,
+  });
+
+  const cvUrl = data.page.cvUpload?.mediaItemUrl || "";
+
+  return {
+    props: {
+      cvUrl,
+    },
+  };
 };
 
 export default CVPage;
--- queries.ts
+++ queries.ts
@@ -3,28 +3,36 @@ import { gql } from '@apollo/client';
 // have combined both getting site title and description with the about information. 
 // will probably do the same for subsequent queries. Will this make loading slower?
 export const GET_SITE_SETTINGS = gql`
-  query GetSiteSettings {
-    generalSettings {
-      title
-      description
+query GetSiteSettings {
+  generalSettings {
+    title
+    description
+  }
+  page(id: "about", idType: URI) {
+    title
+    content
+    featuredImage {
+      node {
+        sourceUrl
+      }
     }
-    page(id: "about", idType: URI) {
-      title
-      content
-      featuredImage {
+    profilePicture {
+      profilePicture {
         node {
           sourceUrl
+          altText
+          id
         }
       }
-      profilePicture {
-        profilePicture {
-          node {
-            sourceUrl
-            altText
-            id
-          }
+    }
+    cvUpload: page(id: "cv", idType: URI) {
+      cvUpload {
+        node {
+          mediaItemUrl
+          mediaType
         }
       }
     }
   }
+}
 `;
